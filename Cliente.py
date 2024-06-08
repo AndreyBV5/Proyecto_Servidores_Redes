36cb4c485314fdd2142a9c50e191109290362d6e
AndreyBV5 <andrey0130player@gmail.com>
2024-06-08 16:05:11 -0600

    Actualizacion mostrar Peso de los videos y libreria tqdm

--- Cliente.py
+++ Cliente.py
@@ -2,6 +2,26 @@
 import socket
 import json
 import argparse
+import time
+from tqdm import tqdm  
+
+def download_video(s, video_name, video_size):
+    with open(video_name, 'wb') as f:
+        downloaded = 0
+        start_time = time.time()
+        
+        with tqdm(total=video_size, unit='B', unit_scale=True, desc=video_name, ascii=True) as pbar:
+            while True:
+                chunk = s.recv(1024)
+                if not chunk:
+                    break
+                f.write(chunk)
+                downloaded += len(chunk)
+                pbar.update(len(chunk))
+        
+        end_time = time.time()
+        print(f"Video descargado como {video_name}")
+        print(f"Tiempo total de descarga: {end_time - start_time:.2f} segundos")
 
 def main():
     parser = argparse.ArgumentParser(description="Cliente para acceder al servidor principal.")
@@ -33,13 +53,13 @@ def main():
             # Mostrar la lista de videos
             print("Lista de videos disponibles:")
             for idx, video in enumerate(video_list, start=1):
-                print(f"{idx}. {video}")
+                print(f"{idx}. {video['name']} - {video['size']} bytes")
 
         if video_number:
             # Solicitar la descarga del video
             s.send(f"download_video {video_number}".encode('utf-8'))
 
-            # Recibir la confirmación con el nombre del video
+            # Recibir la confirmación con el nombre y tamaño del video
             video_data = s.recv(1024).decode('utf-8')
             video_info = json.loads(video_data)
             if "error" in video_info:
@@ -47,14 +67,10 @@ def main():
                 return
 
             video_name = video_info["video_name"]
-            with open(video_name, 'wb') as f:
-                while True:
-                    chunk = s.recv(1024)
-                    if not chunk:
-                        break
-                    f.write(chunk)
+            video_size = video_info["video_size"]
 
-            print(f"Video descargado como {video_name}")
+            # Descargar el video
+            download_video(s, video_name, video_size)
 
         s.close()
     except Exception as e:
